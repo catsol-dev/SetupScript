808e65bb53013dbd0dfe3b50fad5c8c5bb735f2e
Catsol <91440569+catsol-dev@users.noreply.github.com>
2022-06-15 15:14:57 -0600

    Update chrootscript.sh

--- chrootscript.sh
+++ chrootscript.sh
@@ -1,1 +1,25 @@
+#!/bin/bash
 
+# This does most needed stuff in the actual chroot
+echo "Timezone? : "
+read timezone
+ln -sf /usr/share/zoneinfo/$timezone
+hwclock --systohc
+pacman -S --needed --noconfirm neovim base-devel git fish grub efibootmgr os-prober
+nvim /etc/locale.gen
+locale-gen
+touch /etc/locale.conf
+echo "LANG=en_US.UTF-8" > /etc/locale.conf
+touch /etc/hostname
+echo "Hostname? : "
+read user_hostname
+echo $user_hostname > /etc/hostname
+passwd
+echo "Username? : "
+read username
+useradd -m -G wheel,games -s /usr/bin/fish $username
+passwd $username
+cd /home/$username
+git clone https://aur.archlinux.org/paru.git
+grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
+grub-mkconfig -o /boot/grub/grub.cfg
